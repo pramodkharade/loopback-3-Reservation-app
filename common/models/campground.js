8e1c59de56d39c577d047e6f41c4e8294018d3b0
pramodkharade <kharade.pramod91@gmail.com>
2020-04-28 14:38:41 +0530

    custom validation for END date and start date

--- common/models/campground.js
+++ common/models/campground.js
@@ -1,5 +1,5 @@
 'use strict';
 
 module.exports = function(Campground) {
-  Campground.validatesLengthOf('name', {max: 20, message: {max: 'Name is too long'}});
+Campground.validatesLengthOf('name', {max: 100, message: {max: 'Name is too long'}});
 };
--- common/models/reservation.js
+++ common/models/reservation.js
@@ -1,5 +1,10 @@
 'use strict';
 
 module.exports = function(Reservation) {
-
+ Reservation.validate('startDate', dateValidatorFn, {message: 'endDate should be after startDate'});
+  function dateValidatorFn(err) {
+    if (this.startDate >= this.endDate) {
+      err();
+    }
+  }
 };
