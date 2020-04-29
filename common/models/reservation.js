aab83a579af07caaf05f5b3eeffdc5d5cc21432d
pramodkharade <kharade.pramod91@gmail.com>
2020-04-29 12:32:50 +0530

    dnynamic email id of current customer

--- common/models/reservation.js
+++ common/models/reservation.js
@@ -1,10 +1,30 @@
 'use strict';
-
+const Customer = require('../models/customer');
 module.exports = function(Reservation) {
- Reservation.validate('startDate', dateValidatorFn, {message: 'endDate should be after startDate'});
+
+  Reservation.validate('startDate', dateValidatorFn, {
+    message: 'endDate should be after startDate',
+  });
   function dateValidatorFn(err) {
     if (this.startDate >= this.endDate) {
       err();
     }
   }
+  Reservation.observe('after save', function(context, next) {
+    Reservation.app.models.Campground.findById(context.instance.campgroundId,
+      function(err, campground) {
+        Reservation.app.models.Customer.findById(context.instance.userId, function(err, userData) {
+          Reservation.app.models.Email.send({
+            to: userData.email,
+            from: 'Example2@gmail.com',
+            subject: 'Thank you for resveration at ' + campground.name,
+            html: '<p>We confirm your reservation for <strong>' + campground.name + '</strong></p>'
+          }, function(err, mail) {
+            console.log("Error Is::", err);
+            console.log('email sent!');
+          });
+        });
+      });
+    next();
+  });
 };
